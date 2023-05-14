372591a7482e95467eee39cdb9726e78ac3ab297
Seungwoo, Joo <thisis.joos@gmail.com>
2023-05-14 12:35:44 +0900

    feat: initial settings

---
+++ src/main/java/server/api/kiwes/KiwesApplication.java
@@ -0,0 +1,13 @@
+package server.api.kiwes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiwesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiwesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/api/kiwes/KiwesApplicationTests.java
@@ -0,0 +1,13 @@
+package server.api.kiwes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiwesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
