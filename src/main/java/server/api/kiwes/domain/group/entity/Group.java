bf82ed972e13f7f74c0937f73992d212b991d98c
Seungwoo, Joo <thisis.joos@gmail.com>
2023-05-23 19:10:05 +0900

    refactor: modify variable names

--- src/main/java/server/api/kiwes/domain/group/entity/Group.java
+++ src/main/java/server/api/kiwes/domain/group/entity/Group.java
@@ -1,25 +1,30 @@
 package server.api.kiwes.domain.group.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import server.api.kiwes.domain.category.entity.Category;
 import server.api.kiwes.domain.group_member.entity.GroupMember;
 import server.api.kiwes.domain.language.entity.Language;
-import server.api.kiwes.domain.like.entity.Likes;
+import server.api.kiwes.domain.heart.entity.Heart;
 import server.api.kiwes.global.entity.Gender;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 public class Group {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long groupId;
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "GROUP_ID")
+    private Long id;
+    
 
     private String date;
     private String dueTo;
@@ -34,20 +39,21 @@ public class Group {
     private String introduction;
     private String locationKeyword;
     private String location;
-    private String status;
+
+    private Boolean isActivated;
 
 
-    @OneToMany(mappedBy = "group")
-    private List<Category> categoryList = new ArrayList<>();
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Category> categories = new ArrayList<>();
 
-    @OneToMany(mappedBy = "group")
-    private List<Language> languageList = new ArrayList<>();
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Language> languages = new ArrayList<>();
 
-    @OneToMany(mappedBy = "group")
-    private List<GroupMember> groupMemberList = new ArrayList<>();
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<GroupMember> groupMembers = new ArrayList<>();
 
-    @OneToMany(mappedBy = "group")
-    private List<Likes> likesList = new ArrayList<>();
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Heart> hearts = new ArrayList<>();
 
 
 
--- src/main/java/server/api/kiwes/domain/group_member/entity/GroupMember.java
+++ src/main/java/server/api/kiwes/domain/group_member/entity/GroupMember.java
@@ -2,28 +2,31 @@ package server.api.kiwes.domain.group_member.entity;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import server.api.kiwes.domain.group.entity.Group;
 import server.api.kiwes.domain.member.entity.Member;
 
 import javax.persistence.*;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 public class GroupMember {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="group_member_id")
-    private Long groupMemberId;
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "GROUP_MEMBER_ID")
+    private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "group_id")
+    @JoinColumn(name = "GROUP_ID")
     private Group group;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
     private Boolean isHost;
